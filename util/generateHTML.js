15687629f94ec007f583bc867dd5c008fdc471a8
Brigitte Powers <86506686+BrigittePowers@users.noreply.github.com>
2021-10-02 18:02:00 -0500

    Update generateHTML.js

---
+++ util/generateHTML.js
@@ -0,0 +1,15 @@
+<!DOCTYPE>
+<html>
+  <body>
+    <h1>
+      TEAM PROFILE WEBPAGE
+    </h1>
+    <subtitle>A team profile page made successfully with TPG-BP</subtitle>
+  
+  <h2>${team.manager}'s Team</h2>
+  
+  </body>
+  <footer>
+    Made with <3, Din
+  </footer>
+</html>
