61e153ef71c3607a97e942648ea48cc689f42d5e
Brigitte Powers PC <reyhn701@gmail.com>
2021-10-02 11:15:49 -0500

    Arranged folders / created files / basic psuedocoding

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # wk10-team-profile-generator
+
+npm install
+npm inquirer install
+
+-- jest install --
--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+// External required
+const inquirer = require('inquirer');
+const fs = require('fs');
 
+// Internal required
+const html = require('./util/generateHTML,js');
+
+// write file
+function writeToFile(filename, data) {
+    fs.writeFile(filename, data, err => {
+        err ? console.log(err) : console.log('Successfuly page generation!');
+    });
+}
+
+// initialize
+async function init() {
+    try {
+        //ask questions
+        const response = await inquirer.prompt(ask);
+        console.log("Saving Responses...");
+
+        //generate markdown
+        const draft = generateMarkdown(response, git);
+        console.log("Generating README...");
+
+        //write file
+        await writeToFile(`example.html`, draft);
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// run program
+// init();
