141b21f036e09c915f8e3500c7825316f0561396
Brigitte Powers PC <reyhn701@gmail.com>
2021-10-04 22:17:42 -0500

    Added init js and example html

--- index.js
+++ index.js
@@ -3,33 +3,121 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // Internal required
-const html = require('./util/generateHTML,js');
+const Employee = require('./lib/employee');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const Manager = require('./lib/manager');
+const generate = require('./util/generateHTML');
 
-// write file
-function writeToFile(filename, data) {
-    fs.writeFile(filename, data, err => {
-        err ? console.log(err) : console.log('Successfuly page generation!');
-    });
+// Output
+const outputDir = path.resolve(__dirname, "dist");
+const outputPath = path.join(outputDir, "teamprofile.html");
+
+const team = [];
+
+// Inquirer
+function init() {
+    console.log("Thanks for using the Team Profile Generator. Please follow the prompts to build a team. /n /n")
+    
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'mName',
+            message: 'What is the FULL NAME of the manager of this team? (Firstname Lastname)',
+            validate: function (answer) {
+                const regName =  /^[a-zA-Z]+( [a-zA-Z]+)+$/;
+                // input required
+                if (answer.length < 1) {
+                    return console.log("Input required to continue.")
+                // titlecase required
+                } else if (!regName.test(answer)) {
+                    return console.log("Incorrect format (FirstName Lastname required.")
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'mID',
+            message: 'What is their 5-digit employee ID?',
+            validate: function (answer) {
+                if (answer.length < 1) {
+                    return console.log("Input required to continue.")
+                }
+                return true;
+            } 
+        },
+        {
+            type: 'input',
+            name: 'mEmail',
+            message: 'And their email?',
+            validate: function (answer) {
+                if (answer.length < 1) {
+                    return console.log("Input required to continue.")
+                }
+                return true;
+            } 
+        },
+        {
+            type: 'input',
+            name: 'mNum',
+            message: 'And lastly, what is their office number?',
+            validate: function (answer) {
+                if (answer.length < 1) {
+                    return console.log("Input required to continue.")
+                }
+                return true;
+            }
+        }
+    ])
+
+        .then(function (managerRes) {
+            const manager = new Manager(managerRes.mName, managerRes.mID, managerRes.mEmail, managerRes.mNumber);
+            team.push(manager);
+
+            makeTeam();
+
+            function makeTeam() {
+                inquirer.prompt([
+                    // team member selection
+                    {
+                        type: 'list',
+                        message: 'What role do you need to add to your team?',
+                        name: 'employee',
+                        choices: [
+                            "Intern",
+                            "Engineer",
+                            "[Finish Making Team]"
+                        ],
+                    }
+                ])
+                    .then(function (role) {
+                        // INTER
+
+                        if (role.Employee === "Intern") {
+                            console.log('Please answer the following questions to add an intern to the team.')
+                            inquirer.prompt([
+
+                            ])
+                                .then(function (internRes) {
+                                    const intern = new Intern(internRes.iName, internRes.iID, internRes.iEmail, internRes.iSchool);
+                                    team.push(intern);
+                                    makeTeam();
+                                })
+                        // ENGINEER
+                        } else if (role.Employee === "Engineer") {
+                            console.log('Please answer the following questions to add an engineer to the team.')
+                        } else {
+                            // generate html on exit
+                            const draft = generate(team);
+                            fs.writeFile(outputPath, draft, err => {
+                                err ? console.log(err) : console.log('Team profile successfully created!');
+                            });
+                        }
+                    }) 
+            }
+        })
+    
 }
 
-// initialize
-async function init() {
-    try {
-        //ask questions
-        const response = await inquirer.prompt(ask);
-        console.log("Saving Responses...");
-
-        //generate markdown
-        const draft = generateMarkdown(response, git);
-        console.log("Generating README...");
-
-        //write file
-        await writeToFile(`example.html`, draft);
-
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-// run program
-// init();
+init();
--- util/generateHTML.js
+++ util/generateHTML.js
@@ -1,15 +1,69 @@
-<!DOCTYPE>
-<html>
-  <body>
-    <h1>
-      TEAM PROFILE WEBPAGE
-    </h1>
-    <subtitle>A team profile page made successfully with TPG-BP</subtitle>
-  
-  <h2>${team.manager}'s Team</h2>
-  
-  </body>
-  <footer>
-    Made with <3, Din
-  </footer>
-</html>
+const path = require("path");
+const fs = require("fs");
+
+const src = path.resolve(__dirname, "../src");
+
+const generate = employees => {
+  const html = [];
+
+  html.push(...employees
+    .filter(employee => employee.getRole() === "Manager")
+    .map(manager => generateManager(manager))
+    .join("")
+  );
+  html.push(...employees
+    .filter(employee => employee.getRole() === "Engineer")
+    .map(engineer => generateEngineer(engineer))
+    .join("")
+  );
+  html.push(...employees
+    .filter(employee => employee.getRole() === "Intern")
+    .map(intern => generateIntern(intern))
+    .join("")
+  );
+
+  return renderMain(html.join(""));
+
+};
+
+const generateManager = manager => {
+  let template = fs.readFileSync(path.resolve(src, "manager.html"), "utf8");
+  template = replacePlaceholders(template, "name", manager.getName());
+  template = replacePlaceholders(template, "role", manager.getRole());
+  template = replacePlaceholders(template, "email", manager.getEmail());
+  template = replacePlaceholders(template, "id", manager.getId());
+  template = replacePlaceholders(template, "officeNumber", manager.getOfficeNumber());
+  return template;
+};
+
+const generateEngineer = engineer => {
+  let template = fs.readFileSync(path.resolve(src, "engineer.html"), "utf8");
+  template = replacePlaceholders(template, "name", engineer.getName());
+  template = replacePlaceholders(template, "role", engineer.getRole());
+  template = replacePlaceholders(template, "email", engineer.getEmail());
+  template = replacePlaceholders(template, "id", engineer.getId());
+  template = replacePlaceholders(template, "github", engineer.getGithub());
+  return template;
+};
+
+const generateIntern = intern => {
+  let template = fs.readFileSync(path.resolve(src, "intern.html"), "utf8");
+  template = replacePlaceholders(template, "name", intern.getName());
+  template = replacePlaceholders(template, "role", intern.getRole());
+  template = replacePlaceholders(template, "email", intern.getEmail());
+  template = replacePlaceholders(template, "id", intern.getId());
+  template = replacePlaceholders(template, "school", intern.getSchool());
+  return template;
+};
+
+const renderMain = html => {
+  const template = fs.readFileSync(path.resolve(src, "main.html"), "utf8");
+  return replacePlaceholders(template, "team", html);
+};
+
+const replacePlaceholders = (template, placeholder, value) => {
+  const pattern = new RegExp("{{ " + placeholder + " }}", "gm");
+  return template.replace(pattern, value);
+};
+
+module.exports = generate;
